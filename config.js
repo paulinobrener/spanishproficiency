95a5eb6db1c51ec5c723878e66c03899b3dae0db
Paulino Brener <117469041+paulinobrener@users.noreply.github.com>
2025-09-16 19:28:45 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ Example:
 window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/XXXXXXXXXXXXXXXXXXXX/export?format=csv&gid=0";
 Leave it empty to use the local data.csv file.
 */
-window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTnl3wOFh8gAchW82y4WMf_yi_90hgaOeg6YBy2at85pGZHKRQ_-yvN7q9fZ2XnjSg4ac_OTtqFhZac/pub?gid=0&single=true&output=csv"; // paste your CSV URL between quotes
+window.DATA_CSV_URL = ""; // paste your CSV URL between quotes
