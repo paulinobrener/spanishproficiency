bb5fd0842ec63f9ee11d08fefd4957ff24433840
Paulino Brener <117469041+paulinobrener@users.noreply.github.com>
2025-09-16 19:24:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 /*
 Set this to your Google Sheets CSV link for live data.
 Example:
-window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/XXXXXXXXXXXXXXXXXXXX/export?format=csv&gid=0";
+window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/1fxEbHs4gbFC5XnmI1ePHl9HCPqbJ8ZLQs5hem6dYtOA/edit?usp=sharing/export?format=csv&gid=0";
 Leave it empty to use the local data.csv file.
 */
 window.DATA_CSV_URL = ""; // paste your CSV URL between quotes
