3a4542e2e1e9c833a80fa8aa8e795bd3fe7e09aa
Paulino Brener <117469041+paulinobrener@users.noreply.github.com>
2025-09-16 20:31:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.DATA_CSV_URL='';
+window.DATA_CSV_URL='https://docs.google.com/spreadsheets/d/e/2PACX-1vTnl3wOFh8gAchW82y4WMf_yi_90hgaOeg6YBy2at85pGZHKRQ_-yvN7q9fZ2XnjSg4ac_OTtqFhZac/pub?gid=0&single=true&output=csv';
