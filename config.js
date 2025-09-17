b0a7757eb1642947a368205a3471d97b6bedec40
Paulino Brener <117469041+paulinobrener@users.noreply.github.com>
2025-09-16 19:26:08 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-/*
-Set this to your Google Sheets CSV link for live data.
-Example:
-window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/1fxEbHs4gbFC5XnmI1ePHl9HCPqbJ8ZLQs5hem6dYtOA/edit?usp=sharing/export?format=csv&gid=0";
-Leave it empty to use the local data.csv file.
-*/
-window.DATA_CSV_URL = ""; // paste your CSV URL between quotes
