f5ad92a3ebba8857e686e804a3f57f1f980e4a0c
Paulino Brener <117469041+paulinobrener@users.noreply.github.com>
2025-09-16 19:26:47 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+
+/*
+Set this to your Google Sheets CSV link for live data.
+Example:
+window.DATA_CSV_URL = "https://docs.google.com/spreadsheets/d/XXXXXXXXXXXXXXXXXXXX/export?format=csv&gid=0";
+Leave it empty to use the local data.csv file.
+*/
+window.DATA_CSV_URL = ""; // paste your CSV URL between quotes
